f02d1dcd061a3c3800dc9910a933059e25324805
yurun <miao.work2010@gmail.com>
2018-01-11 21:03:27 +0800

    Add auto commit offset

--- kafka/src/main/java/com/yurun/kafka/consumer/KafkaConsumerExample.java
+++ kafka/src/main/java/com/yurun/kafka/consumer/KafkaConsumerExample.java
@@ -21,6 +21,8 @@ public class KafkaConsumerExample {
     properties
         .put("bootstrap.servers", "d013057201.dip.weibo.com:9092,d013057202.dip.weibo.com:9092");
     properties.put("group.id", "yurun");
+    properties.put("enable.auto.commit", "true");
+    properties.put("auto.commit.interval.ms", "1000");
     properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     properties
         .put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
